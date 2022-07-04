fc12b557c9eb2283fc0ffb19ad0ce3b72d266b79
Clovis pereira da Silva <clovis.eduardosilva23@gmail.com>
2022-07-04 11:10:11 -0300

    primeiro commit
    Subindo a automação cartas

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon"
+export const IMAGE_API_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+//export const IMAGEM_API_URL_LOCAL = "https://w7.pngwing.com/pngs/779/474/png-transparent-inspector-clouseau-cartoon-the-pink-panther-television-show-inspecter-cartoon-bird-inspector-clouseau-thumbnail.png"
+//export const POKEMON_API_URL = "https://i722h3koghxg4qjagnbarr7kgi0droax.lambda-url.us-east-1.on.aws/"
